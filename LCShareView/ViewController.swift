32f381c047fff38a7c19b41df8d0030135268094
licong <1240690490@qq.com>
2016-09-18 18:18:16 +0800

    class ViewController: UIViewController {

--- LCShareView/ViewController.swift
+++ LCShareView/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var tableView:UITableView = {
+        let tableView = UITableView(frame: UIScreen.main.bounds, style: .plain)
+        tableView.tableFooterView = UIView(frame: .zero)
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    let cellTitleArray = ["淡入淡出", "滑动", "自定义Presentation"]
+    let cellSubtitleArray = ["淡入淡出的动画", "交互式切换","自定义弹出框"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "自定义动画"
+        view.addSubview(tableView)
+        
     }
+    
 }
+
+
+extension ViewController:UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
 
